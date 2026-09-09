d1d4862fa4b353c131a597f84e3632eeb63c5bfc
fscard <fscard@gmail.com>
2026-09-08 20:13:51 -0400

    Fix fork condition check for child process

--- fork_example.c
+++ fork_example.c
@@ -12,7 +12,7 @@
 int main() {
 
     pid_t pid = fork();  // Cria um processo filho (duplica o processo atual).
-    if (fork() == 0) {
+    if (pid == 0) {
         // ================================
         // BLOCO DO FILHO
         // ================================
