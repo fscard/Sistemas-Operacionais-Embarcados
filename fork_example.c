071590d73669a405c8029e9743d1ee78ac6ec458
fscard <fscard@gmail.com>
2026-09-08 20:11:55 -0400

    Update fork_example.c

--- fork_example.c
+++ fork_example.c
@@ -10,41 +10,17 @@
 #include <string.h>   // strlen
 
 int main() {
-    int fd[2];          // descritores de arquivo do pipe: fd[0] = leitura, fd[1] = escrita
-    char buffer[50];    // buffer para armazenar a mensagem lida
 
-    // Cria um pipe anônimo (só existe enquanto os processos estiverem vivos).
-    // pipe(fd) preenche fd[0] (leitura) e fd[1] (escrita).
-    if (pipe(fd) == -1) {
-        perror("Erro ao criar pipe");
-        return 1;
-    }
-
-    // Cria um processo filho (duplica o processo atual).
+    pid_t pid = fork();  // Cria um processo filho (duplica o processo atual).
     if (fork() == 0) {
         // ================================
         // BLOCO DO FILHO
         // ================================
-        close(fd[0]); // o filho não vai ler, então fecha o descritor de leitura
-
-        char msg[] = "Mensagem do filho!";
-        // escreve no pipe: vai para fd[1]
-        // strlen(msg)+1 inclui o '\0' no final da string
-        write(fd[1], msg, strlen(msg)+1);
-
-        close(fd[1]); // fecha o descritor de escrita (boa prática)
+        printf("Sou processo filho! PID=%d\n", getpid());
     } else {
         // ================================
         // BLOCO DO PAI
         // ================================
-        close(fd[1]); // o pai não vai escrever, então fecha o descritor de escrita
-
-        // lê do pipe (bloqueia até o filho escrever algo)
-        read(fd[0], buffer, sizeof(buffer));
-
-        // imprime a mensagem recebida do filho
-        printf("Pai recebeu: %s\n", buffer);
-
-        close(fd[0]); // fecha o descritor de leitura
+        printf("Sou processo Pai! PID=%d, filho =%d \n", getpid(), pid);
     }
 }
